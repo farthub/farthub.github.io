ec469db620d9539e23da864cd6461796a8ba26da
Sindura Sriram <sindura_sriram@brown.edu>
2021-07-01 18:55:50 -0700

    debug 5.3

--- load-index.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    var smile_animation = document.getElementById("t3");
-    var icons = document.getElementsByClassName("icons")[0];
-
-    setTimeout(function () {
-        icons.className += " loaded";
-        smile_animation.beginElement();
-    }, 4000);
-};
