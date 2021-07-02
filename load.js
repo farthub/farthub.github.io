8c4fcc9b6f5447b2c8431bdae6b4971421d1398a
Sindura Sriram <sindura_sriram@brown.edu>
2021-07-01 19:08:03 -0700

    debug 6.1

--- load.js
+++ load.js
@@ -2,9 +2,10 @@ function loadIndex() {
     var smile_animation = document.getElementById("t3");
     var icons = document.getElementsByClassName("icons")[0];
 
-    icons.className += " loaded";
-    smile_animation.beginElement();
-
+    setTimeout(function () {
+        icons.className += " loaded";
+        smile_animation.beginElement();
+    }, 4000);
 }
 
 // window.onload = loadIndex;
@@ -19,7 +20,7 @@ document.onreadystatechange = function () {
             document.getElementsByClassName('load')[0].style.visibility = "hidden";
             document.getElementById('contents').style.visibility = "visible";
             var fileName = location.pathname.split("/").slice(-1);
-            if (fileName[0] === "index.html") {
+            if(fileName[0] === "index.html"){
                 loadIndex();
             }
         }, 2000);
