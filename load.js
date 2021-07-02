d5393d242c488f35105a4201668af089342b2ae1
Sindura Sriram <sindura_sriram@brown.edu>
2021-07-01 18:51:33 -0700

    debug 5.1

--- load.js
+++ load.js
@@ -1,17 +1,29 @@
-document.onreadystatechange = function () {
-    var state = document.readyState
-    if (state == 'interactive') {
-        document.getElementById('contents').style.visibility = "hidden";
-    } else if (state == 'complete') {
+function loadIndex(){
+    window.onload = function () {
+        var smile_animation = document.getElementById("t3");
+        var icons = document.getElementsByClassName("icons")[0];
+    
         setTimeout(function () {
-            document.getElementById('interactive');
-            document.getElementsByClassName('load')[0].style.visibility = "hidden";
-            document.getElementById('contents').style.visibility = "visible";
-            var fileName = location.pathname.split("/").slice(-1);
-            if(fileName[0] === "index.html"){
-                loadIndex();
-            }
-        }, 2000);
-        
-    }
+            icons.className += " loaded";
+            smile_animation.beginElement();
+        }, 4000);
+    };
 }
+
+// document.onreadystatechange = function () {
+//     var state = document.readyState
+//     if (state == 'interactive') {
+//         document.getElementById('contents').style.visibility = "hidden";
+//     } else if (state == 'complete') {
+//         setTimeout(function () {
+//             document.getElementById('interactive');
+//             document.getElementsByClassName('load')[0].style.visibility = "hidden";
+//             document.getElementById('contents').style.visibility = "visible";
+//             var fileName = location.pathname.split("/").slice(-1);
+//             if(fileName[0] === "index.html"){
+//                 loadIndex();
+//             }
+//         }, 2000);
+        
+//     }
+// }
