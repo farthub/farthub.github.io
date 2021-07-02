b7adf4c7bc3ebfffcbbbc5ae81e3d8abc5ba2bb4
Sindura Sriram <sindura_sriram@brown.edu>
2021-07-01 19:03:32 -0700

    debug 6

--- load.js
+++ load.js
@@ -2,28 +2,27 @@ function loadIndex() {
     var smile_animation = document.getElementById("t3");
     var icons = document.getElementsByClassName("icons")[0];
 
-    setTimeout(function () {
-        icons.className += " loaded";
-        smile_animation.beginElement();
-    }, 4000);
+    icons.className += " loaded";
+    smile_animation.beginElement();
+
 }
 
-window.onload = loadIndex;
+// window.onload = loadIndex;
 
-// document.onreadystatechange = function () {
-//     var state = document.readyState
-//     if (state == 'interactive') {
-//         document.getElementById('contents').style.visibility = "hidden";
-//     } else if (state == 'complete') {
-//         setTimeout(function () {
-//             document.getElementById('interactive');
-//             document.getElementsByClassName('load')[0].style.visibility = "hidden";
-//             document.getElementById('contents').style.visibility = "visible";
-//             var fileName = location.pathname.split("/").slice(-1);
-//             if(fileName[0] === "index.html"){
-//                 loadIndex();
-//             }
-//         }, 2000);
+document.onreadystatechange = function () {
+    var state = document.readyState
+    if (state == 'interactive') {
+        document.getElementById('contents').style.visibility = "hidden";
+    } else if (state == 'complete') {
+        setTimeout(function () {
+            // document.getElementById('interactive');
+            document.getElementsByClassName('load')[0].style.visibility = "hidden";
+            document.getElementById('contents').style.visibility = "visible";
+            var fileName = location.pathname.split("/").slice(-1);
+            if (fileName[0] === "index.html") {
+                loadIndex();
+            }
+        }, 2000);
 
-//     }
-// }
+    }
+}
